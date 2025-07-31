3ac04dfc567bb79ed3e195a123bc97853abe3ab2
msattova <msattova@gmail.com>
2025-07-31 14:14:20 +0900

    BrowserRouterを用いて開発環境とそれ以外とでbasenameが切り替わるように

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter
+    basename={
+      import.meta.env.DEV ? "/" : "/deckMakerCF/"
+    }
+  >
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://msattova.github.io/deckMakerCF/",
   plugins: [react(), visualizer()],
   build: {
     rollupOptions: {

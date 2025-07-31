611070516bdb30ba46e3824d77642200d71c4d45
msattova <msattova@gmail.com>
2025-07-31 14:36:07 +0900

    baseに/を指定

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), visualizer()],
   build: {
     rollupOptions: {
